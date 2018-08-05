b37d8098385a74312d9355f91febab7645a043c2
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2018-08-04 19:08:32 -0700

    Rendering slave app by passing JSON

--- src/components/SlaveApp.js
+++ src/components/SlaveApp.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import '../css/SlaveApp.css';
 
 class SlaveApp extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        let button_style=JSON.parse(this.props.inputValue);
+        console.log("data-");
+        console.log(button_style);
+
+
         return (
             <div className="SlaveApp">
                 <p>
                     Slave App
                 </p>
+
+                <p>
+                    <button style={button_style}>Slave App Button</button>
+
+                </p>
+
             </div>
         );
     }
